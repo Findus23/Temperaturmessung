b5614d32a886ec887fa9a6afae4792e034aa4540
Findus23 <l.winkler23@me.com>
2014-01-24 17:00:27 +0100

    Code aufräumen
    * alte Programmteile, die nicht mehr benötigt werden gelöscht
    * Kommentare hinzugefügt

--- export.sh
+++ export.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 zufall=0
-PFAD="/var/www/"
-Anzahl=0
-Summe=0
+PFAD="/var/www/" #Pfad zum Web-Verzeichnis
+Anzahl=0 #statistische Auswertung
+Summe=0 #statistische Auswertung
 min=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-00080277abe1/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l) # Sowohl Minimum als auch Maximum auf die aktuelle Temperatur setzen
 max=$min
 r=0 # Backup-Zahl auf Null setzen
 IFS="; " #Spezial-Variable, enthält Trennzeichen zum Trennen von Luftdruck und -temperatur
-if [ $1 ]
+if [ $1 ] # if- und case- Abfrage für Startparameter
 then
 	case "$1" in
 		"-d") echo "" > rohdaten.csv
@@ -29,59 +29,55 @@ do
 	#load=$(cut -c 1,2,3,4 /proc/loadavg) # Load messen
 	rasp=$(/opt/vc/bin/vcgencmd measure_temp | cut -c 6,7,8,9) #Betriebstemberatur messen
 	#cpu=$(sensors |grep Core\ 0 |cut -c 18,19,20,21) #CPU-Temperatur, lm-sensors muss installiert sein, bei jedem PC anders
-	temp1=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-00080277abe1/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l)
+	temp1=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-00080277abe1/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l) #Temperatursensor auf Steckbrett
 	while [ "$temp1" == "-1.250" ]
 	do
 		echo "----Temp1: $temp1"
 		temp1=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-00080277abe1/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l)
 	done
-	temp2=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-00080277a5db/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l)
+	temp2=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-00080277a5db/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l) #Temperatursensor auf Steckbrett
 	while [ "$temp2" == "-1.250" ]
 	do
 		echo "----Temp2: $temp2"
 		temp2=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-00080277a5db/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l)
 	done
-	temp3=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-000802b4635f/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l)
+	temp3=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-000802b4635f/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l) #Außensensor
 	while [ "$temp3" == "-1.250" ]
 	do
 		echo "----Temp3: $temp3"
-		temp3=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-000802b4635f/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l)
+		temp3=$(echo "scale=3; $(grep 't=' /sys/bus/w1/devices/w1_bus_master1/10-000802b4635f/w1_slave | awk -F 't=' '{print $2}') / 1000" | bc -l) 
 	done
-	luft_roh=$(sudo ./Fremddateien/Adafruit_DHT 2302 17 |grep Hum )	
+	luft_roh=$(sudo ./Fremddateien/Adafruit_DHT 2302 17 |grep Hum )	# Rohdaten des Luftfeuchtigkeits-Sensors
 	while [ -z "$luft_roh" ] 
 	do
 		echo "----Luft: $luft_roh"
 		luft_roh=$(sudo ./Fremddateien/Adafruit_DHT 2302 17 |grep Hum )
 	done
-	luft_temp=$(echo $luft_roh | cut -c 8,9,10,11)
+	luft_temp=$(echo $luft_roh | cut -c 8,9,10,11) # Luftfeuchtigkeit-Sensor auftrennen
 	luft_feucht=$(echo $luft_roh | cut -c 23,24,25,26)
-	druck_roh=$(sudo python Fremddateien/Adafruit_BMP085_auswertung.py)
+	druck_roh=$(sudo python Fremddateien/Adafruit_BMP085_auswertung.py) # Rohdaten des Luftdruck-Sensors
 	set -- $druck_roh #Zerlegen mithilfe von IFS (siehe ganz oben)
 	temp_druck=$1
 	druck=$2
-	uhrzeit=$(date +%H:%M:%S) 
-	uhrzeit_dy=$(date +%Y/%m/%d\ %H:%M:%S)
+	uhrzeit=$(date +%Y/%m/%d\ %H:%M:%S)
 
 #Mathematische Auswertung Anfang
-	Summe=$(echo "$Summe + $temp1" | bc -l) # mithilfe von bc den aktuellen Wert zur Summe aller Werten dazuzählen ...
-	Anzahl=$(($Anzahl +1)) # ... die Anzahl um 1 erhöhen ...
-	MW=$(echo "scale=3;$Summe / $Anzahl" | bc -l) # ... und den Mittelwert berechnen
-	if [ "$(echo "$temp1 < $min" | bc -l)" = "1" ] # Falls der aktuelle Wert kleiner als das Minimum ist ...
-		then
-			min=$temp1								#  ... soll er zum neuen Minimum werden
-	fi
-	if [ "$(echo "$temp1 > $max" | bc -l)" = "1" ] # Wie Minimum
-		then
-			max=$temp1
-	fi
-	
+#	Summe=$(echo "$Summe + $temp1" | bc -l) # mithilfe von bc den aktuellen Wert zur Summe aller Werten dazuzählen ...
+#	Anzahl=$(($Anzahl +1)) # ... die Anzahl um 1 erhöhen ...
+#	MW=$(echo "scale=3;$Summe / $Anzahl" | bc -l) # ... und den Mittelwert berechnen
+#	if [ "$(echo "$temp1 < $min" | bc -l)" = "1" ] # Falls der aktuelle Wert kleiner als das Minimum ist ...
+#		then
+#			min=$temp1								#  ... soll er zum neuen Minimum werden
+#	fi
+#	if [ "$(echo "$temp1 > $max" | bc -l)" = "1" ] # Wie Minimum
+#		then
+#			max=$temp1
+#	fi
+#	
 #Mathematische Auswertung Ende
-	ausgabe=${uhrzeit}\,${temp1}\,${temp2}
-	ausgabe_dy=${uhrzeit_dy}\,${temp1}\,${temp2}\,${temp3}\,${luft_temp}\,${luft_feucht}\,${druck}\,${temp_druck}\,${rasp}
-	echo $ausgabe >>rohdaten.csv
-	echo $ausgabe_dy >>dygraph.csv
-	echo "$uhrzeit_dy	${temp1},${temp2},${temp3},${luft_temp},${luft_feucht},${druck},${temp_druck},${rasp}" #Ausgabe des aktuellen Wertes im Terminal
-#	sed "s/,/ /g" dygraph.csv >daten_gnuplot.txt #für Gnuplot die Beistriche durch Leerzeichen ersetzen 
+	ausgabe=${uhrzeit_dy}\,${temp1}\,${temp2}\,${temp3}\,${luft_temp}\,${luft_feucht}\,${druck}\,${temp_druck}\,${rasp}
+	echo $ausgabe >>dygraph.csv
+	echo "$uhrzeit	${temp1},${temp2},${temp3},${luft_temp},${luft_feucht},${druck},${temp_druck},${rasp}" #Ausgabe des aktuellen Wertes im Terminal
 	echo "Uhrzeit:" >text.txt #Anzeigen für Display 
 	echo "$uhrzeit" >>text.txt
 	echo "Geraetetemp 1" >>text.txt
@@ -98,16 +94,13 @@ do
 	echo "$luft_feucht" >>text.txt
 	echo "Prozessor" >>text.txt
 	echo "$rasp" >>text.txt
-#	./transpose.sh #anderes Skript starten, welches die Daten für Highchart vorbereitet
-#	gnuplot Einstellungen.plt # Gnuplot starten
-#	sudo cp gnuplot.svg ${PFAD}gnuplot.svg # das generierte Bild ...
-#	sudo cp daten_transformiert.txt ${PFAD}daten_transformiert.txt # ... und die Tabelle für Highchart in den Webordner kopieren
 	sudo cp dygraph.csv ${PFAD}dygraph.csv
 	sleep 8 # kurz warten
-	r=$(($r +1))
-	if [ "$r" == "1000" ]
+	r=$(($r +1)) # Anzahl der Durchläufe zählen
+	if [ "$r" == "1000" ] # und alle 1000 Durchgänge Sicherung anfertigen
 	then
-		cp rohdaten.csv dygraph.csv daten_gnuplot.txt text.txt backup/
+		cp dygraph.csv text.txt backup/
 		echo "Backup"
+		r=0
 	fi
 done
