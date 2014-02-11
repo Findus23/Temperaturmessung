f963bc6337e323ba342587a99732ac7424e53c20
Findus23 <l.winkler23@me.com>
2014-02-11 16:38:01 +0100

    Standardabweichung allgemein

--- Endauswertung/mathe.py
+++ Endauswertung/mathe.py
@@ -26,6 +26,16 @@ def minmax(spalte):
 			mini = wert
 	return (mini,maxi)
 
+def standardabweichung(spalte,mw):
+	n = 0
+	summe = 0
+	for wert in spalte:
+		term = wert - mw
+		summe = summe + (term * term)
+		n += 1
+	stab = math.sqrt(summe / n)
+	return stab
+	
 offnen("vorbereitet.csv")
 print("------Mittelwerte------")
 mw_temp1 = mittelwert(temp1)
@@ -47,6 +57,7 @@ print("Luftfeuchtigkeit:\t%0.2f" % mw_luft_feucht)
 print("Luftdruck:\t\t%0.2f" % mw_druck)
 print("Temperatur (Druck):\t%0.2f" % mw_temp_druck)
 print("Prozessor:\t\t%0.2f" % mw_rasp)
+
 print("------Minimum-Maximum------")
 min_temp1 = minmax(temp1)[0]
 max_temp1 = minmax(temp1)[1]
@@ -65,8 +76,7 @@ max_druck = minmax(druck)[1]
 min_temp_druck = minmax(temp_druck)[0]
 max_temp_druck = minmax(temp_druck)[1]
 min_rasp = minmax(rasp)[0]
-max_rasp = minmax(rasp)[0]
-
+max_rasp = minmax(rasp)[1]
 print("Innentemperatur:\t" + str(min_temp1) + "\t" + str(max_temp1))
 print("Gerätetemperatur 1:\t" + str(min_temp2) + "\t" + str(max_temp2))
 print("Außentemperatur:\t" + str(min_temp3) + "\t" + str(max_temp3))
@@ -77,13 +87,22 @@ print("Luftdruck:\t\t" + str(min_druck) + "\t" + str(max_druck))
 print("Temperatur (Druck):\t" + str(min_temp_druck) + "\t" + str(max_temp_druck))
 print("Prozessor:\t\t" + str(min_rasp) + "\t" + str(max_rasp))
 
-n = 0
-summe = 0
-for wert in temp1:
-	zwischen = wert - mw_temp1
-	z_quadrat = zwischen * zwischen
-	summe = summe + z_quadrat
-	n += 1
-emp_roh = summe / n
-emp = math.sqrt(emp_roh)
-print(emp)
+print("------Standardabweichung------")
+stab_temp1 = standardabweichung(temp1,mw_temp1)
+stab_temp2 = standardabweichung(temp2,mw_temp2)
+stab_temp3 = standardabweichung(temp3,mw_temp3)
+stab_temp4 = standardabweichung(temp4,mw_temp4)
+stab_luft_temp = standardabweichung(luft_temp,mw_luft_temp)
+stab_luft_feucht = standardabweichung(luft_feucht,mw_luft_feucht)
+stab_druck = standardabweichung(druck,mw_druck)
+stab_temp_druck = standardabweichung(temp_druck,mw_temp_druck)
+stab_rasp = standardabweichung(rasp,mw_rasp)
+print("Innentemperatur:\t%0.2f" % stab_temp1)
+print("Gerätetemperatur 1:\t%0.2f" % stab_temp2)
+print("Außentemperatur:\t%0.2f" % stab_temp3)
+print("Gerätetemperatur 2:\t%0.2f" % stab_temp4)
+print("Temperatur (Luft):\t%0.2f" % stab_luft_temp)
+print("Luftfeuchtigkeit:\t%0.2f" % stab_luft_feucht)
+print("Luftdruck:\t\t%0.2f" % stab_druck)
+print("Temperatur (Druck):\t%0.2f" % stab_temp_druck)
+print("Prozessor:\t\t%0.2f" % stab_rasp)
