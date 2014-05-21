485fbed109d69225549d7265dec34ba3482ed02d
Findus23 <l.winkler23@me.com>
2014-05-21 13:58:32 +0200

    mitternacht.sh verbessert

--- diverses/mitternacht.sh
+++ diverses/mitternacht.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+date > /home/pi/log/mitternacht.log
 pushbullet_api_key=$(cat /home/pi/Temperaturmessung/Fremddateien/pushbullet_settings.txt | head -n 1)
 pushbullet_device=$(cat /home/pi/Temperaturmessung/Fremddateien/pushbullet_settings.txt | tail -n 1)
 cd /home/pi/Temperaturmessung
-sudo -H -u pi git pull origin master
-# cp /home/pi/Temperaturmessung/Webinterface /var/www
-sudo apt-get update
-sudo apt-get dist-upgrade > /home/pi/log/mitternacht.log
+sudo -H -u pi git pull origin master >> /home/pi/log/mitternacht.log 2>&1
+cp -r /home/pi/Temperaturmessung/Webinterface /var/www
+echo "\n\n\n" >> /home/pi/log/mitternacht.log
+sudo apt-get update >> /home/pi/log/mitternacht.log
+echo "\n\n\n" >> /home/pi/log/mitternacht.log
+sudo apt-get dist-upgrade -y >> /home/pi/log/mitternacht.log
+echo "\n\n\n" >> /home/pi/log/mitternacht.log
+sudo apt-get autoremove -y >> /home/pi/log/mitternacht.log
+echo "\n\n\n" >> /home/pi/log/mitternacht.log
+sudo apt-get clean >> /home/pi/log/mitternacht.log
+
+date >> /home/pi/log/mitternacht.log
 /home/pi/Temperaturmessung/Fremddateien/pushbullet_cmd.py $pushbullet_api_key file $pushbullet_device /home/pi/log/mitternacht.log
-/sbin/shutdown -r +5
+sudo /sbin/shutdown -r +5
