0f736eb3a029465cef3d872a3360c03bebdcf85b
Findus23 <l.winkler23@me.com>
2014-06-01 15:15:53 +0200

    mitternacht.sh: Log-Dateien nach Datum benannt

--- diverses/mitternacht.sh
+++ diverses/mitternacht.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-date > /home/pi/log/mitternacht.txt
+datum=$(date +%d.%m.%y)
+date > /home/pi/log/mitternacht_$datum.txt
 pushbullet_api_key=$(cat /home/pi/Temperaturmessung/Fremddateien/pushbullet_settings.txt | head -n 1)
 pushbullet_device=$(cat /home/pi/Temperaturmessung/Fremddateien/pushbullet_settings.txt | tail -n 1)
 cd /home/pi/Temperaturmessung
-sudo -H -u pi git pull origin master >> /home/pi/log/mitternacht.txt 2>&1
+sudo -H -u pi git pull origin master >> /home/pi/log/mitternacht_$datum.txt 2>&1
 cp -r /home/pi/Temperaturmessung/Webinterface/* /var/www/
-echo "\n\n\n" >> /home/pi/log/mitternacht.txt
-sudo apt-get update >> /home/pi/log/mitternacht.txt
-echo "\n\n\n" >> /home/pi/log/mitternacht.txt
-sudo apt-get dist-upgrade -y >> /home/pi/log/mitternacht.txt
-echo "\n\n\n" >> /home/pi/log/mitternacht.txt
-sudo apt-get autoremove -y >> /home/pi/log/mitternacht.txt
-echo "\n\n\n" >> /home/pi/log/mitternacht.txt
-sudo apt-get clean >> /home/pi/log/mitternacht.txt
+echo "\n\n\n" >> /home/pi/log/mitternacht_$datum.txt
+sudo apt-get update >> /home/pi/log/mitternacht_$datum.txt
+echo "\n\n\n" >> /home/pi/log/mitternacht_$datum.txt
+sudo apt-get dist-upgrade -y >> /home/pi/log/mitternacht_$datum.txt
+echo "\n\n\n" >> /home/pi/log/mitternacht_$datum.txt
+sudo apt-get autoremove -y >> /home/pi/log/mitternacht_$datum.txt
+echo "\n\n\n" >> /home/pi/log/mitternacht_$datum.txt
+sudo apt-get clean >> /home/pi/log/mitternacht_$datum.txt
 
-date >> /home/pi/log/mitternacht.txt
-/home/pi/Temperaturmessung/Fremddateien/pushbullet_cmd.py $pushbullet_api_key file $pushbullet_device /home/pi/log/mitternacht.txt
+date >> /home/pi/log/mitternacht_$datum.txt
+/home/pi/Temperaturmessung/Fremddateien/pushbullet_cmd.py $pushbullet_api_key file $pushbullet_device /home/pi/log/mitternacht_$datum.txt
 sudo /sbin/shutdown -r +5
