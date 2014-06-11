1e4ea9c34b0ba7687c6ad1225515ca51f7d72ce1
Findus23 <l.winkler23@me.com>
2014-06-11 13:45:06 +0200

    Crontab aktualisiert

--- diverses/ftp.sh
+++ diverses/ftp.sh
@@ -2,4 +2,4 @@ host=$(cat /home/pi/Temperaturmessung/diverses/ftp_settings.txt | head -n 1)
 benutzername=$(cat /home/pi/Temperaturmessung/diverses/ftp_settings.txt | head -n 2 | tail -n 1)
 passwort=$(cat /home/pi/Temperaturmessung/diverses/ftp_settings.txt | tail -n 1)
 cd /home/pi/Temperaturmessung/
-ncftpput -V -m -u $benutzername -p $passwort $host / dygraphs.csv text_ws.txt
+ncftpput -V -m -u $benutzername -p $passwort $host / dygraph.csv text_ws.txt
