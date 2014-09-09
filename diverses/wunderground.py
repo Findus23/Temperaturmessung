9c8384b498d2240ea90eba0cffc5d008f97d8be7
Findus23 <l.winkler23@me.com>
2014-09-09 11:45:57 +0200

    Weather U. Fehler ausgebessert

--- diverses/wunderground.py
+++ diverses/wunderground.py
@@ -22,10 +22,11 @@ gertemp2 = 9.0/5.0 * gertemp2_c + 32
 lufttemp = 9.0/5.0 * lufttemp_c + 32
 aussentemp = 9.0/5.0 * aussentemp_c + 32
 processor = 9.0/5.0 * processor_c + 32
-druck_hg = druck * 0.02961339710085
+druck_hg = druck * 0.02956
 zeit=datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 pfad="http://rtupdate.wunderground.com/weatherstation/updateweatherstation.php?ID=" + wu_id + "&PASSWORD=" + password + "&dateutc=" + zeit + "&tempf=" + str(aussentemp) + "&temp2f=" + str(bodentemp) + "&temp2f=" + str(lufttemp) + "&baromin=" + str(druck_hg) + "&humidity=" + str(luftfeucht) + "&indoortempf=" + str(innentemp) + "&softwaretype=raspberry_pi&action=updateraw&realtime=1&rtfreq=30"
 r = requests.get(pfad)
 if (r.text == "success\n") & (r.status_code == 200):
+	print("ok")
 else:
 	print("Ein Fehler ist aufgetreten: " + r.text)
